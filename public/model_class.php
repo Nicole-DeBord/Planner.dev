26fc7cc44d5279e27d852dfa9f4c4b2377e737f4
Nicole DeBord <a.nicole.debord@gmail.com>
2015-01-27 10:36:01 -0600

    model class for address book db

---
+++ public/model_class.php
@@ -0,0 +1,23 @@
+<?php
+
+class Model {
+
+	protected $dbc;
+	public $id;
+
+	public function __construct($dbc) {
+		$this->dbc = $dbc;
+	}
+	
+	public function save() {
+		if (isset($this->id)) {
+			return $this->update();
+		} else {
+			return $this->$insert();
+		}
+	}
+}
+
+
+
+?>
